d18da835e3d53498d880755449fb99d886ee56e7
Baxtiyor <baxtiyorshukurjanov36@gmail.com>
2022-03-29 02:27:33 +0300

    added form that get data

---
+++ is-valid-palindrome/src/components/Input.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Input = () => {
+    return (
+        <div>
+            <form>
+                <input type="text" />
+            </form>
+        </div>
+    )
+}
+
+export default Input
